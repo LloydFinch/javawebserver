f39985823cfb9df3c39942bedfdf4ded3a9821d5
lloydfinch <venn2devloper2mail@gmail.com>
2018-10-21 22:27:42 +0800

    add hello world code

---
+++ server/helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		sayHello();
+	}
+
+	public static void sayHello() {
+		String msg = "hello world";
+		System.out.println(msg);
+	}
+}
